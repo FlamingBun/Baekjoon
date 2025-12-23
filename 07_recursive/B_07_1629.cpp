b2e2265cbeb467f7ea7cd9eef00e9ae6319abbb4
FlamingBun <wh180911@gmail.com>
2025-12-23 17:23:11 +0900

    solve : 1629

---
+++ 07_recursive/B_07_1629.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+
+using namespace std;
+
+using ll = long long;
+
+ll POW(ll a, ll b, ll m)
+{
+    if(b == 1) return a % m;
+    
+    ll val = POW(a, b/2, m);
+    
+    val = val * val % m;
+    
+    if(b%2 == 0) return val;
+    else return val * a % m;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    ll a, b, c;
+    
+    cin >> a >> b >> c;
+    
+    cout << POW(a, b, c) << '\n';
+}
